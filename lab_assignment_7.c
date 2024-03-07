61e25c581141dab8a92c33ca5e1d903ae3db7aa6
Donaldo Martinez <do073496@ucf.edu>
2024-03-07 14:19:10 -0500

    Update lab_assignment_7.c
    Created the bubble sort function

--- lab_assignment_7.c
+++ lab_assignment_7.c
@@ -9,18 +9,40 @@ int main(void) {
     int arr_one[9] = {97, 16, 45, 63, 13, 22, 7, 58, 72};
     int arr_two[9] = {90, 80, 70, 60, 50, 40, 30, 20, 10};
 
-    selection_sort(arr_one, 9);
-    selection_sort(arr_two, 9);
+    // selection_sort(arr_one, 9);
+    bubble_sort(arr_one, 9);
+
+    // selection_sort(arr_two, 9);
+    bubble_sort(arr_two, 9);
 
     return 0;
 }
 
 int bubble_sort(int arr[], int size) {
-    for (int i = 0; i < size; i++) {
-        for (int j = 0; j < size - i; j++) {
+    int total_swaps = 0;
+    int value_swap_count[size];
+    initialize_array(value_swap_count, size);
 
+    for (int i = 0; i < size; i++) {
+        int index = 0;
+        for (int j = 0; j < size - i; j++) {
+            if (arr[index] > arr[j]) {
+                int temp = arr[index];
+                value_swap_count[index] += 1;
+                arr[index] = arr[j];
+                value_swap_count[j] += 1;
+                arr[j] = temp;
+                index = j;
+                total_swaps++;
+            }
         }
     }
+
+    printf("Swaps: %d\n", total_swaps);
+    for (int i = 0; i < size; i++) {
+        printf("Index %d   Value: %d   Swap Count: %d\n", i, arr[i], value_swap_count[i]);
+    }
+    printf("\n\n");
     return 0;
 }
 
@@ -37,13 +59,15 @@ int selection_sort(int arr[], int size) {
         // For every element after the sorted elements
         for (int j = i; j < size; j++) {
 
-            // If 
+            // If the minimum value stored is greater than the current element's value
+            // then change the minimum value to be the current value and store the index of that value
             if (min > arr[j]) {
                 min = arr[j];
                 index = j;
             }
         }
 
+        // If there was an update to our index, swap the minimum value to the starting value of this iteration
         if (index > i) {
             arr[index] = arr[i];
             value_swap_count[index] += 1;
@@ -55,7 +79,7 @@ int selection_sort(int arr[], int size) {
 
     printf("Swaps: %d\n", total_swaps);
     for (int i = 0; i < size; i++) {
-            printf("Index %d   Value: %d   Swap Count: %d\n", i, arr[i], value_swap_count[i]);
+        printf("Index %d   Value: %d   Swap Count: %d\n", i, arr[i], value_swap_count[i]);
     }
     printf("\n\n");
     
