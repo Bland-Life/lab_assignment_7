3b55af00632e9716fa4dc6b1f65a6f36ca294abb
Donaldo Martinez <do073496@ucf.edu>
2024-03-07 21:24:20 -0500

    Update lab_assignment_7.c
    Adjusted bubble sort function to output correct number of swaps total. Also added a function to print the array.

--- lab_assignment_7.c
+++ lab_assignment_7.c
@@ -1,19 +1,37 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int bubble_sort(int arr[], int size);
 int selection_sort(int arr[], int size);
 void initialize_array(int arr[], int size);
+void print_array(int arr[], int size);
 
 int main(void) {
+    
     int arr_one[9] = {97, 16, 45, 63, 13, 22, 7, 58, 72};
     int arr_two[9] = {90, 80, 70, 60, 50, 40, 30, 20, 10};
+    int testing_duplicate[9];
 
-    // selection_sort(arr_one, 9);
-    bubble_sort(arr_one, 9);
+    
 
-    // selection_sort(arr_two, 9);
-    bubble_sort(arr_two, 9);
+    printf("Selection Sort: \n\n");
+    memcpy(testing_duplicate, arr_one, sizeof(arr_one));
+    selection_sort(testing_duplicate, 9);
+    print_array(testing_duplicate, 9);
+
+    memcpy(testing_duplicate, arr_two, sizeof(arr_two));
+    selection_sort(testing_duplicate, 9);
+    print_array(testing_duplicate, 9);
+
+    printf("Bubble Sort: \n\n");
+    memcpy(testing_duplicate, arr_one, sizeof(arr_one));
+    bubble_sort(testing_duplicate, 9);
+    print_array(testing_duplicate, 9);
+
+    memcpy(testing_duplicate, arr_two, sizeof(arr_two));
+    bubble_sort(testing_duplicate, 9);
+    print_array(testing_duplicate, 9);
 
     return 0;
 }
@@ -24,15 +42,13 @@ int bubble_sort(int arr[], int size) {
     initialize_array(value_swap_count, size);
 
     for (int i = 0; i < size; i++) {
-        int index = 0;
         for (int j = 0; j < size - i; j++) {
-            if (arr[index] > arr[j]) {
-                int temp = arr[index];
-                value_swap_count[index] += 1;
-                arr[index] = arr[j];
+            if (j + 1 < size && arr[j] > arr[j+1]) {
+                int temp = arr[j];
                 value_swap_count[j] += 1;
-                arr[j] = temp;
-                index = j;
+                arr[j] = arr[j + 1];
+                value_swap_count[j + 1] += 1;
+                arr[j + 1] = temp;
                 total_swaps++;
             }
         }
@@ -42,7 +58,7 @@ int bubble_sort(int arr[], int size) {
     for (int i = 0; i < size; i++) {
         printf("Index %d   Value: %d   Swap Count: %d\n", i, arr[i], value_swap_count[i]);
     }
-    printf("\n\n");
+    printf("\n");
     return 0;
 }
 
@@ -81,7 +97,7 @@ int selection_sort(int arr[], int size) {
     for (int i = 0; i < size; i++) {
         printf("Index %d   Value: %d   Swap Count: %d\n", i, arr[i], value_swap_count[i]);
     }
-    printf("\n\n");
+    printf("\n");
     
     return 0;
 }
@@ -90,4 +106,11 @@ void initialize_array(int arr[], int size) {
     for (int i = 0; i < size; i++) {
         arr[i] = 0;
     }
+}
+
+void print_array(int arr[], int size) {
+    for (int i = 0; i < size; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n\n-----------------------------------------\n");
 }
